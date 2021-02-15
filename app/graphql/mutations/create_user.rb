492383661fd79e95573f536bad256423c0f42762
kazuki-sumi <cs5200@icloud.com>
2021-02-15 23:19:42 +0900

    user作成のmutation追加

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,20 @@
+module Mutations
+  class CreateUser < BaseMutation
+    graphql_name 'CreateUser'
+    field :user, Types::UserType, null: true
+    field :result, Boolean, null: true
+
+    argument :name, String, required: true
+    argument :email, String, required: true
+    argument :password, String, required: true
+    argument :role, Integer, required: false
+
+    def resolve(**args)
+      user = User.create(name: args[:name], email: args[:email], password: args[:password], role: args[:role])
+      {
+        user: user,
+        result: user.errors.blank?
+      }
+    end
+  end
+end
