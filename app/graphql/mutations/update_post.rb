c556fd7a11463e1a35f849a105ccf887a1f72872
kazuki-sumi <cs5200@icloud.com>
2021-02-18 21:05:18 +0900

    mutation update_post修正

--- app/graphql/mutations/update_post.rb
+++ app/graphql/mutations/update_post.rb
@@ -8,10 +8,11 @@ module Mutations
     argument :id, ID, required: true
     argument :title, String, required: false
     argument :description, String, required: false
+    argument :status, Integer, required: false
 
     def resolve(**args)
       post = Post.find(args[:id])
-      post.update(title: args[:title], description: args[:description])
+      post.update(title: args[:title], description: args[:description], status: args[:status])
       {
         post: post,
         result: post.errors.blank?
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,7 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
     # field :delete_post, mutation: Mutations::DeletePost
-    # field :update_post, mutation: Mutations::UpdatePost
+    field :update_post, mutation: Mutations::UpdatePost
     field :create_post, mutation: Mutations::CreatePost
   end
 end
