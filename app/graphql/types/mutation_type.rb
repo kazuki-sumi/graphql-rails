fb72a39fe54d20d556c77eaa1e5855ab730e5337
kazuki-sumi <cs5200@icloud.com>
2021-04-22 08:11:56 +0900

    fix

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
-    field :sign_in_user, mutation: Mutations::SignInUser
+    field :signin_user, mutation: Mutations::SignInUser
     field :create_user, mutation: Mutations::CreateUser
     field :delete_post, mutation: Mutations::DeletePost
     field :update_post, mutation: Mutations::UpdatePost
