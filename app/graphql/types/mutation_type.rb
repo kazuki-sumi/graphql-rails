891b27c11ead66cfb3a656dc8f0577adbf36708e
kazuki-sumi <cs5200@icloud.com>
2021-05-03 22:19:56 +0900

    vote作成

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :create_vote, mutation: Mutations::CreateVote
     field :create_link, mutation: Mutations::CreateLink
     field :signin_user, mutation: Mutations::SignInUser
     field :create_user, mutation: Mutations::CreateUser
