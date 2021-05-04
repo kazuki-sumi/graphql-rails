991403826fb35c8b24bcd8992b30f11173eda89f
kazuki-sumi <cs5200@icloud.com>
2021-05-04 17:56:00 +0900

    関連付け

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,6 +6,8 @@ module Types
     field :password_digest, String, null: false
     field :role, Integer, null: true
     field :posts, [Types::PostType], null: false
+    field :links, [Types::LinkType], null: false
+    field :votes, [Types::VoteType], null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts, dependent: :destroy
   has_many :liks, dependent: :destroy
+  has_many :votes
+  has_many :links
 
   enum role: [ :banned, :reader, :sub_admin, :admin ]
   validates :name, presence: true
